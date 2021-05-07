97877d927eee75d05deca1c78845f6a831a7b575
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-05-07 18:58:43 +0900

    Update: LV1_문자열내마음대로정렬하기

--- Programmers_Swift/LV1_문자열내마음대로정렬하기.swift
+++ Programmers_Swift/LV1_문자열내마음대로정렬하기.swift
@@ -10,17 +10,9 @@ import Foundation
 /// [문자열 내 마음대로 정렬하기]
 struct LV1_문자열내마음대로정렬하기 {
     func solution(_ strings:[String], _ n:Int) -> [String] {
-        var arr = strings.sorted() // 미리 사전순으로 정렬
-        
-        // n번째 글자 기준으로 정렬
-        for i in 0..<arr.count {
-            for j in 0..<arr.count - (i + 1) {
-                if Array(arr[j])[n] > Array(arr[j + 1])[n] {
-                    arr.swapAt(j, j + 1)
-                }
-            }
-        }
-        
-        return arr
+        let arr = strings.sorted() // 미리 사전순으로 정렬
+    
+        // n번째 문자 기준으로 정렬
+        return arr.sorted {Array($0)[n] < Array($1)[n]}
     }
 }
