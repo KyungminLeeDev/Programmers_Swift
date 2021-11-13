1a642ae3d087717c769c4327a704dbfffc4aca89
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-11-13 21:58:37 +0900

    [#1] LV2_가장큰수

--- Programmers_Swift/LV2_가장큰수.swift
+++ Programmers_Swift/LV2_가장큰수.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import XCTest
 
 /// [가장 큰 수](https://programmers.co.kr/learn/courses/30/lessons/42746)
 struct LV2_가장큰수 {
@@ -18,5 +19,48 @@ struct LV2_가장큰수 {
         }
         
         return result.joined()
+    }
+}
+
+class LV2_가장큰수_Tests: XCTestCase {
+    private struct TestCase {
+        let numbers: [Int]
+        let output: String
+    }
+    
+    private var sut: LV2_가장큰수!
+    private var testCase: TestCase!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = LV2_가장큰수()
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+        testCase = nil
+        try super.tearDownWithError()
+    }
+    
+    func testCase1() {
+        // 1. given
+        testCase = TestCase(numbers: [6, 10, 2], output: "6210")
+        
+        // 2. when
+        let output = sut.solution(testCase.numbers)
+        
+        // 3. then
+        XCTAssertEqual(output, testCase.output)
+    }
+    
+    func testCase2() {
+        // 1. given
+        testCase = TestCase(numbers: [3, 30, 34, 5, 9], output: "9534330")
+        
+        // 2. when
+        let output = sut.solution(testCase.numbers)
+        
+        // 3. then
+        XCTAssertEqual(output, testCase.output)
     }
 }
